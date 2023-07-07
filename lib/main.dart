8a14b953bc7fecddfd6e7637e8babcf1541eb7b9
Anirudh V Gubbi <anirudhvgubbi@gmail.com>
2023-07-07 13:38:25 +0530

    initialised hive local storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:linkemo/core/designs.dart';
 import 'package:linkemo/features/home/presentation/home_page.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
   runApp(const MyApp());
 }
 
