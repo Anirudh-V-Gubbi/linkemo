52eab5be1dbcb2731138380e17726a06ad7da233
Anirudh V Gubbi <anirudhvgubbi@gmail.com>
2023-07-09 16:48:41 +0530

    updated dependencies

--- lib/features/home/presentation/bloc/link_home_bloc.dart
+++ lib/features/home/presentation/bloc/link_home_bloc.dart
@@ -1,17 +1,24 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:linkemo/features/home/domain/usecase/get_all_link_details.dart';
+import 'package:linkemo/features/home/domain/usecase/store_link_details.dart';
 
 part 'link_home_event.dart';
 part 'link_home_state.dart';
 part 'link_home_bloc.freezed.dart';
 
 class LinkHomeBloc extends Bloc<LinkHomeEvent, LinkHomeState> {
-  LinkHomeBloc() : super(const LinkHomeState.loading());
+  final GetAllLinkDetails getAllLinkDetails;
+  final StoreLinkDetails storeLinkDetails;
 
-  Stream<LinkHomeState> mapEventToState(LinkHomeEvent event) async*{
-    if(event is _GetAllLinkDetailsEvent) {
+  LinkHomeBloc({
+    required this.getAllLinkDetails,
+    required this.storeLinkDetails,
+  }) : super(const LinkHomeState.loading());
+
+  Stream<LinkHomeState> mapEventToState(LinkHomeEvent event) async* {
+    if (event is _GetAllLinkDetailsEvent) {
       yield const LinkHomeState.loading();
     }
   }
-
 }
